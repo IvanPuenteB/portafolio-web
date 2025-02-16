d12ea7a3cfd89107d66773fa185d7ecd4cf31474
IvanPuenteB <148298119+IvanPuenteB@users.noreply.github.com>
2025-02-16 16:05:28 -0600

    agregue el menu para mobiles

--- js/index.js
+++ js/index.js
@@ -1,5 +1,19 @@
 let download = document.getElementById('button-download')
 
+const abrir = document.querySelector('.menu-moviles'); // Botón de abrir menú
+const nav = document.querySelector('.nav-list'); // Menú
+const cerrar = document.querySelector('#cerrar'); // Botón de cerrar menú
+
+// Función para abrir el menú
+abrir.addEventListener("click", () => {
+    nav.classList.add("abrir"); // Aplica la clase con animación
+});
+
+// Función para cerrar el menú
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("abrir"); // Oculta el menú con animación
+});
+
 console.log(download)
 
 download.addEventListener("click", (e) => {
